a8bad0f8e79f60dc589d1c856b5ae456910d81eb
harmanparihar <harmanparihar18@gmail.com>
2018-10-03 16:42:41 -0700

    Calculator app basic functions implemented

---
+++ js/components/Buttons.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Number from './Number.jsx'
+export default class Buttons extends React.Component{
+
+  render(){
+    return(
+      <div className="num-container">
+      <Number buttonpress={this.props.buttonpress} val={"AC"} classname={"num op-1"}/>
+      <Number buttonpress={this.props.buttonpress} val={"+/-"} classname={"num op-1"}/>
+      <Number buttonpress={this.props.buttonpress} val={"%"} classname={"num op-1"}/>
+      <Number buttonpress={this.props.buttonpress} val={"\u00F7"} classname={"num op"}/>
+      <Number buttonpress={this.props.buttonpress} val={7} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={8} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={9} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={"\u00D7"} classname={"num op"}/>
+      <Number buttonpress={this.props.buttonpress} val={5} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={4} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={6} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={"-"} classname={"num op"}/>
+      <Number buttonpress={this.props.buttonpress} val={1} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={2} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={3} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={"+"} classname={"num op"}/>
+      <Number buttonpress={this.props.buttonpress} val={0} classname={"num zero"}/>
+      <Number buttonpress={this.props.buttonpress} val={"."} classname={"num"}/>
+      <Number buttonpress={this.props.buttonpress} val={"="} classname={"num op"}/>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+const webpack =require("webpack");
+module.exports ={
+  entry: './js/index.js',
+  output: {path: __dirname, filename: 'webpack_bundle.js'},
+  module:{
+    loaders:[
+      { test: /\.css$/,
+           loader: "style-loader!css-loader"
+      },
+      {
+        test: /.jsx?$/,
+        loader:'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015','react']
+        }
+      },
+    ]
+  }
+};
